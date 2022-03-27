e30854b2172056be5e1737c1d7203b313e4ac5f3
Stefan Strauberg <stefanstrauberg@gmail.com>
2022-03-27 16:08:53 +0300

    Initial Front

---
+++ src/FrontEnd/Client/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthComponent } from './auth/auth.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    AuthComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

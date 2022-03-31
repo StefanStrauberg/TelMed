85e778f8a4b49e759009b8a69c2c717b74fc5844
Stefan Strauberg <stefanstrauberg@gmail.com>
2022-03-31 10:04:53 +0300

    DbTestServer and model Organization

---
+++ src/FrontEnd/Client/src/app/models/IOrganization.ts
@@ -0,0 +1,37 @@
+export interface Organization {
+    id: string;
+    published: Date;
+    updated: Date;
+    level: OrganizationLevel;
+    region: OrganizationRegion;
+    address: Address;
+    isActive: boolean;
+    organizationName: OrganizationName;
+    specializationIds: string[];
+}
+
+export enum OrganizationLevel {
+    AreaLevel,
+    RegionLevel,
+    RepublicLevel
+}
+
+export enum OrganizationRegion {
+    BrestRegion,
+    VitebskRegion,
+    GomelRegion,
+    GrodnoRegion,
+    MinskRegion,
+    MogilevRegion,
+    Minsk,
+    Belarus
+}
+
+export interface Address {
+    line: string;
+}
+
+export interface OrganizationName {
+    usualName: string;
+    officialName: string;
+}

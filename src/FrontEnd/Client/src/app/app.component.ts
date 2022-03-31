ba9fc2f33d0fc328107cdd99f4a0c6d1daf6d9fc
Stefan Strauberg <stefanstrauberg@gmail.com>
2022-03-31 23:47:06 +0300

    Create Authorize

--- src/FrontEnd/Client/src/app/app.component.ts
+++ src/FrontEnd/Client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Client';
+export class AppComponent implements OnInit {
+  
+  userAuthenticated = false;
+
+  constructor(private _authService: AuthService){
+    this._authService.loginChanged.subscribe(userAuthenticated => {
+      this.userAuthenticated = userAuthenticated;
+    })
+  }
+
+  ngOnInit(): void {
+    this._authService.isAuthenticated().then(userAuthenticated => {
+      this.userAuthenticated = userAuthenticated;
+    })
+  }
 }

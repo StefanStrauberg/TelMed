d775692d5a7bf0d3ec88280dfadbc2a3a0dc4b07
Stefan Strauberg <stefanstrauberg@gmail.com>
2022-04-22 14:44:34 +0300

    fix

--- src/Services/Anamnesies/CORE/Anamnesies.Application/DTO/UpdateAnamnesisDto.cs
+++ src/Services/Anamnesies/CORE/Anamnesies.Application/DTO/UpdateAnamnesisDto.cs
@@ -4,7 +4,6 @@ namespace Anamnesies.Application.DTO
 {
     public class UpdateAnamnesisDto
     {
-        public string Id { get; set; }
         public string ReferralId { get; set; }
         public AnamnesisCategory CategoryId { get; set; }
         public string Summary { get; set; }

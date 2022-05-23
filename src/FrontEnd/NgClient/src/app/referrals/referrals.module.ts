d8cd5f81944f08b67a921f794579e285471488ab
Stefan Strauberg <stefanstrauberg@gmail.com>
2022-05-23 17:09:47 +0300

    working datepicker for create referral component

--- src/FrontEnd/NgClient/src/app/referrals/referrals.module.ts
+++ src/FrontEnd/NgClient/src/app/referrals/referrals.module.ts
@@ -11,6 +11,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
     ReactiveFormsModule,
     MatInputModule,
     MatSelectModule,
-    MatDatepickerModule
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   exports: [
     ViewReferralsComponent
